c49cb041c64e8a5f34a0c9b0d67fff6b978e8958
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2025-06-02 21:15:35 +0300

    add precommit hook, project files, server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4078;
+const fs = require("fs");
+
+app.use(express.static(path.join(__dirname, "build/static")));
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
